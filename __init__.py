1285eb8f0b0627364bfa7ba8bf33dd9dd66579ca
gxj <24172280@qq.com>
2021-09-22 16:41:31 +0800

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+class Share(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions['share'] = self
